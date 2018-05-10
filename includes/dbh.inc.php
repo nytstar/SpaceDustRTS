b095591031cbe32619c207548304b844f0b61fcb
Imperial7 <bobo201345@gmail.com>
2018-05-11 02:33:21 +0300

    Restarting from Scratch

--- includes/dbh.inc.php
+++
@@ -1,8 +0,0 @@
-<?php
-$servername = "localhost";
-$username = "id5651497_admin";
-$password = "Clinton15Jason";
-$database = "id5651497_spacedust";
-
-$conn = mysqli_connect( $servername , $username , $password , $database );
-
